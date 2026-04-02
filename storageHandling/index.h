f78afa6d55d7316d164f0c8a41c2b239a848c636
Tristan Mangin <tw.mangin@gmail.com>
2026-04-01 21:33:08 -0400

    index definition safety

--- storageHandling/index.h
+++ storageHandling/index.h
@@ -5,6 +5,8 @@
 // getStagedHash() -> string
 // clear()
 // hasStaged() -> bool
+#ifndef INDEX_H
+#define INDEX_H
 
 #include "hasher.h"
 #include "utils.h"
@@ -13,3 +15,5 @@ void stage(const std::filesystem::path &repoRoot, const std::string &hash);
 std::string getStagedHash(const std::filesystem::path &repoRoot);
 void clear(const std::filesystem::path &repoRoot);
 bool hasStaged(const std::filesystem::path &repoRoot);
+
+#endif // INDEX_H
