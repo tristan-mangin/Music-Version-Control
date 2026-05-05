6d8a8b20433ca54fd8e318fe4b83a671b234d600
Tristan Mangin <tw.mangin@gmail.com>
2026-05-05 11:27:09 -0400

    django admin setup

--- bvcs_backend/bvcs/admin.py
+++ bvcs_backend/bvcs/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Repository, Commit
 
-# Register your models here.
+@admin.register(Repository)
+class RepositoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "path", "created_at"]
+    search_fields = ["name"]
+    ordering = ["name"]
+
+@admin.register(Commit)
+class CommitAdmin(admin.ModelAdmin):
+    list_display = ["hash_short", "repository", "message", "timestamp"]
+    search_fields = ["hash", "message"]
+    list_filter = ["repository"]
+    ordering = ["-timestamp"]
+
+    def hash_short(self, obj):
+        return obj.hash[:8]
+    hash_short.short_description = "Hash"
